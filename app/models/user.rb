476f2f9f65e66fe67bf670ad93e7739be9365251
Rui Pedro Barata <ruipbarata@gmail.com>
2020-11-23 20:12:19 +0000

    214. Adicionando dados extra

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+
   validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
   def full_name
