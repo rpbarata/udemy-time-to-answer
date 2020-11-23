16c93fcc39bb91f56d9153e0feba691521b37cb9
Rui Pedro Barata <ruipbarata@gmail.com>
2020-11-23 22:44:45 +0000

    222. Melhorando a UX

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -9,9 +9,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      if params_user[:user_profile_attributes][:avatar]
-        redirect_to users_backoffice_welcome_index_path, notice: "Avatar actualizado com sucesso"
-      else
+      unless params_user[:user_profile_attributes][:avatar]
         redirect_to users_backoffice_profile_path, notice: "Utilizador actualizado com sucesso"
       end
     else
